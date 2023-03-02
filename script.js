de655a93ef99fa67fbf5f48a5e69bb7c2ac60788
Shuvam <shanta.mitra.sm@gmail.com>
2023-03-02 19:21:02 +0530

    anekta kaaj korlam wow

--- script.js
+++ script.js
@@ -1,8 +1,96 @@
-image = document.querySelector('.screen-background-1 #gadha');
+alignDivs = () => {
+    lmao = (800 - window.innerHeight) / 4;
+    imageDivs = document.getElementsByClassName('bg-image');
 
-lmao = (800 - window.innerHeight)/4;
+    if (window.innerHeight < 800) {
+        for (let i = 0; i < imageDivs.length; i++) {
 
-if (window.innerHeight < 800) {
-    image.style.transform = 'translateY(-' + String(lmao) + 'px)';
+            imageDivs[i].style.transform = 'translateY(-' + String(lmao) + 'px)';
+            console.log(imageDivs[i])
+        }
+    }
+
+    bottomJinish = document.querySelector('.pack-2');
+    bottomJinish.style.bottom = String((window.innerHeight - 600) / 2 + 10) + 'px';
+
+    thoughtJinish = document.querySelector('.pack-3');
+    thoughtJinish.style.paddingTop = String((300 - thoughtJinish.offsetHeight) / 2) + 'px';
+
+    authorJinish = document.querySelector('.pack-4');
+    authorJinish.style.bottom = String((window.innerHeight - 550) / 2 + 10) + 'px';
 }
-console.log("lmao")
+
+updateTime = () => {
+    var d = new Date();
+    days = { 0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday" }
+    months = { 0: "Jan", 1: "Feb", 2: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" }
+    numbers = { 0: "00", 1: "01", 2: "02", 3: "03", 4: "04", 5: "05", 6: "06", 7: "07", 8: "08", 9: "09", 10: "10", 11: "11", 12: "12", 13: "13", 14: "14", 15: "15", 16: "16", 17: "17", 18: "18", 19: "19", 20: "20", 21: "21", 22: "22", 23: "23", 24: "24", 25: "25", 26: "26", 27: "27", 28: "28", 29: "29", 30: "30", 31: "31", 32: "32", 33: "33", 34: "34", 35: "35", 36: "36", 37: "37", 38: "38", 39: "39", 40: "40", 41: "41", 42: "42", 43: "43", 44: "44", 45: "45", 46: "46", 47: "47", 48: "48", 49: "49", 50: "50", 51: "51", 52: "52", 53: "53", 54: "54", 55: "55", 56: "56", 57: "57", 58: "58", 59: "59" }
+
+    dayInfo = document.querySelector(".day-info");
+    timeInfo = document.querySelector(".time-info");
+
+    timeInfo.innerText = `${numbers[d.getHours()]}:${numbers[d.getMinutes()]}`;
+    dayInfo.innerText = `${days[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
+}
+
+puttingInfo = () => {
+
+    openWeatherKey = "cc46fd7fb64a54521d5fdb91ba060264";
+    openWeatherKey2 = "c5fdebb786aa4c1218a28a1c7395df26";
+    var city = "Kolkata"
+    var lat = 22.5726;
+    var lon = 88.3639;
+
+    // getting location 
+    url1 = "http://worldtimeapi.org/api/ip";
+    fetch(`${url1}`).then(response => {
+        return response.json();
+    }).then(data => {
+        city = data.timezone.split("/")[1];
+        console.log(city);
+    })
+
+    url2 = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${openWeatherKey}`;
+    fetch(`${url2}`).then(response => {
+        return response.json();
+    }).then(data => {
+        lat = data[0].lat;
+        lon = data[0].lon;
+    })
+
+    url3 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherKey}}`
+    // fetch(`${url3}`).then(response => {
+    //     return response.json();
+    // }).then(data => {
+    //     // console.log(data);
+    //     // kaaj korche navigator, pore dekhchi
+    // })
+}
+
+showerThoughts = () => {
+    thoughtDiv = document.querySelector('.pack-3-thought')
+    authorDiv = document.querySelector('.pack-4-thought-author')
+    authorLink = document.querySelector('.author-link')
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'edad73df64msh41d1a22add27a66p166a78jsn6f3b61ba2379',
+            'X-RapidAPI-Host': 'stapi-showerthoughts.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://stapi-showerthoughts.p.rapidapi.com/api/v1/stapi/top', options)
+        .then(response => response.json())
+        .then(data => {
+            thoughtDiv.innerText = data.data[0].showerthought;
+            authorDiv.innerText = data.data[0].user;
+            authorLink.href = `https://www.reddit.com/user/${data.data[0].user.slice(2)}`
+        }).catch(err => console.error(err));
+}
+
+alignDivs();
+puttingInfo();
+showerThoughts();
+
+setInterval(updateTime, 900)
